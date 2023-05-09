2955387dd07e6801aed937b5d400de6d32c0d289
Jian-Nam <jeeahn2929@gmail.com>
2023-05-09 21:26:35 +0900

    Feat: 백엔드로 이미지 업로드,백엔드에서 멀티 디텍션

---
+++ App.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const multer  = require('multer');
+const util = require('util');
+const tf = require('@tensorflow/tfjs');
+const tfNode = require('@tensorflow/tfjs-node');
+const fs = require("fs");
+
+const app = express();
+
+const path = require('path');
+const upload = multer({ 
+    storage: multer.diskStorage({
+        // set a localstorage destination
+        destination: (req, file, callback) => {
+            callback(null, 'uploads/');
+        },
+        // convert a file name
+        filename: (req, file, callback) => {
+            callback(null, new Date().valueOf() + path.extname(file.originalname));
+        },
+      }),
+ });
+
+const PORT = process.env.PORT || 5000;
+
+//유저가 보낸 array/object 데이터를 출력해보기 위해 필요
+app.use(express.json()); 
+
+//다른 도메인주소끼리 ajax 요청 주고받을 때 필요
+app.use(cors()); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello here is server");
+    console.log("Hello");
+});
+
+
+const yolov5_weight = 'https://raw.githubusercontent.com/Jian-Nam/YOLOv5_model/main/model.json'
+
+
+
+app.post("/api/files/images", upload.single('img'),  async(req, res) => {
+    const image_path = req.file.path;
+    // console.log(req.file)
+
+ 
+    const buffer =  fs.readFileSync(image_path)
+    // console.log(typeof buffer)
+
+    imageTensor =  tfNode.node.decodeImage(buffer)
+
+
+    const detector = await tf.loadGraphModel(yolov5_weight);
+    let [modelWidth, modelHeight] =  detector.inputs[0].shape.slice(1, 3)
+    converted_img = tf.image.resizeBilinear(imageTensor, [modelWidth, modelHeight]).expandDims(0)
+    detect_res = await detector.executeAsync(converted_img)
+    
+    const [boxes, scores, classes, valid_detections] = detect_res
+    console.log(boxes.dataSync())
+    
+    if(image_path === undefined) {
+        return res.status(400).send( "failed")
+    }
+    res.status(200).send("success");
+});
+
+app.listen(PORT, (err) => {
+    if (err) return console.log(err);
+    else console.log("SERVER STARTS IN", PORT);
+});
