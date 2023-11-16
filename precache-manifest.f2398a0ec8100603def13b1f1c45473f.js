1b0e7bb9e611323584cc2a0b5a7b4b6090f056d9
Jian-Nam <jeeahn2929@gmail.com>
2023-11-16 22:14:16 +0900

    Updates

---
+++ precache-manifest.f2398a0ec8100603def13b1f1c45473f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ade0fbde6ffe7aa88b601cd1b958db",
+    "url": "/DecorDetector/index.html"
+  },
+  {
+    "revision": "009da5628fbaee395c5b",
+    "url": "/DecorDetector/static/css/main.7ca04aea.chunk.css"
+  },
+  {
+    "revision": "faeccbbf9ff5bc3a2c5f",
+    "url": "/DecorDetector/static/js/2.8528ad28.chunk.js"
+  },
+  {
+    "revision": "ea7ded9f1f5094e14392",
+    "url": "/DecorDetector/static/js/3.d433891d.chunk.js"
+  },
+  {
+    "revision": "009da5628fbaee395c5b",
+    "url": "/DecorDetector/static/js/main.4a7ddc79.chunk.js"
+  },
+  {
+    "revision": "b9bd593c026037b7f66a",
+    "url": "/DecorDetector/static/js/runtime~main.20d80ccd.js"
+  }
+]);
